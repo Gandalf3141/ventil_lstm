ee28135dc35ed12041e092e007b0e98c949a6198
Gandalf3141 <paul.strasser98@gmail.com>
2024-05-13 16:16:16 +0200

    new vers of future steps

---
+++ testing.py
@@ -0,0 +1,18 @@
+from ray import train, tune
+
+
+def objective(config):  # ①
+    score = config["a"] ** 2 + config["b"]
+    return {"score": score}
+
+
+search_space = {  # ②
+    "a": tune.choice([0.001, 0.01, 0.1, 1.0]),
+    
+    "b": tune.choice([1, 2, 3]),
+}
+
+tuner = tune.Tuner(objective, param_space=search_space, tune_config=tune.TuneConfig(num_samples=10))  # ③
+
+results = tuner.fit()
+print(results.get_best_result(metric="score", mode="min").config)
