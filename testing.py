b8aa5510e6b726f63e1384b4137fabe9a065ac63
Gandalf3141 <paul.strasser@ait.ac.at>
2024-05-21 09:20:59 +0200

    new method of training lstms with new forward method in model class

--- testing.py
+++ testing.py
@@ -1,68 +1,13 @@
-"""This example demonstrates the usage of BayesOpt with Ray Tune.
-
-It also checks that it is usable with a separate scheduler.
-
-Requires the BayesOpt library to be installed (`pip install bayesian-optimization`).
-"""
-import time
-
-from ray import train, tune
-from ray.tune.schedulers import AsyncHyperBandScheduler
-from ray.tune.search import ConcurrencyLimiter
-from ray.tune.search.bayesopt import BayesOptSearch
-from ray.tune.search.optuna import OptunaSearch
 
 
-def evaluation_fn(step, width, height):
-    return (0.1 + width * step / 100) ** (-1) + height * 0.1
+res = 1
 
+n = 2000
+ 
+for i in range(1,n):
 
-def easy_objective(config):
-    # Hyperparameters
-    width, height = config["width"], config["height"]
+    res = res * i
 
-    for step in range(config["steps"]):
-        # Iterative training function - can be any arbitrary training procedure
-        intermediate_score = evaluation_fn(step, width, height)
-        # Feed the score back back to Tune.
-        train.report({"iterations": step, "mean_loss": intermediate_score})
-        time.sleep(0.1)
+print(res)
 
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--smoke-test", action="store_true", help="Finish quickly for testing"
-    )
-    args, _ = parser.parse_known_args()
-
-    #algo = BayesOptSearch(metric="mean_loss", mode="min", utility_kwargs={"kind": "ucb", "kappa": 2.5, "xi": 0.0})
-    algo = OptunaSearch(metric="mean_loss", mode="min")
-
-
-   # algo = ConcurrencyLimiter(algo, max_concurrent=3)
-    scheduler = AsyncHyperBandScheduler()
-    tuner = tune.Tuner(
-        easy_objective,
-        tune_config=tune.TuneConfig(
-            metric="mean_loss",
-            mode="min",
-            search_alg=algo,
-            scheduler=scheduler,
-            num_samples=40 #if args.smoke_test else 1000,
-
-        ),
-        run_config=train.RunConfig(
-            name="my_exp",
-        ),
-        param_space={
-            "steps": 100,
-            "width": tune.randint(0, 20),
-            "height": tune.choice([-100, -90,-80,-70,-60,-50,-40, 100]),
-        },
-    )
-    results = tuner.fit()
-
-    print("Best hyperparameters found were: ", results.get_best_result().config)
+print((n*(n+1)/2))
