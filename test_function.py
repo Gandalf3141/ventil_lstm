d193bd570b24cdedf286de0e4e72306c851b144f
Gandalf3141 <paul.strasser98@gmail.com>
2024-06-03 10:04:50 +0200

    best NN yet

--- test_function.py
+++ test_function.py
@@ -24,9 +24,11 @@ def plot_results(x, pred, pred_next_step=None, physics_rescaling=None):
         # x * (xmax - xmin) + xmin
 
         pred[:,0] = pred[:,0]*(physics_rescaling[0] - physics_rescaling[3]) + physics_rescaling[3]
+        pred[:,0] = pred[:,0]/1e5
         pred[:,1] = pred[:,1]*(physics_rescaling[1] - physics_rescaling[4]) + physics_rescaling[4]
         pred[:,2] = pred[:,2]*(physics_rescaling[2] - physics_rescaling[5]) + physics_rescaling[5]
         x[:,0] = x[:,0]*(physics_rescaling[0] - physics_rescaling[3]) + physics_rescaling[3]
+        x[:,0] = x[:,0]/1e5
         x[:,1] = x[:,1]*(physics_rescaling[1] - physics_rescaling[4]) + physics_rescaling[4]
         x[:,2] = x[:,2]*(physics_rescaling[2] - physics_rescaling[5]) + physics_rescaling[5]
 
