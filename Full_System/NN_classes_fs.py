82d0aab82e5d55420ef54e7db430f5a3286070eb
Gandalf3141 <paul.strasser98@gmail.com>
2024-08-01 16:07:52 +0200

    .

---
+++ Full_System/NN_classes_fs.py
@@ -0,0 +1,278 @@
+# NN Classes used for simulation of the full system
+
+
+import torch
+from torch import nn
+from torch.nn.utils import weight_norm
+import torchcde
+
+# strg + k + 1/2
+
+#############################################################################################
+###                  >>>       OR - derivative prediction       <<<                       ###
+#############################################################################################
+
+# # OR - LSTM
+# class OR_LSTM(nn.Module):
+
+#     def __init__(self, input_size, hidden_size, out_size, layers, window_size=4):
+
+#         super().__init__()
+
+#         self.hidden_size = hidden_size
+#         self.input_size = input_size
+#         self.ws = window_size
+        
+#         # Define LSTM layer
+#         self.lstm = nn.LSTM(input_size, hidden_size, num_layers=layers, batch_first=True)
+
+#         # Define linear layer
+#         self.linear = nn.Linear(hidden_size, out_size)
+
+#     def forward(self, one_full_traj):
+
+#         seq = one_full_traj[:, 0:self.ws, :]
+#         lstm_out, hidden = self.lstm(seq)           
+#         pred = self.linear(lstm_out)
+#         #only update next step
+#         out = one_full_traj[:, self.ws-1:self.ws, 1:] + pred[:, -1: , :]
+
+#         for t in range(1, self.ws): # für RK : range(1, self.ws + 2):
+
+#             tmp = torch.cat((one_full_traj[:,self.ws+(t-1):self.ws+(t-1)+(out.size(dim=1)), 0:1] , out[:, :, :]), dim=2)
+#             seq = torch.cat((one_full_traj[:, t:self.ws, :], tmp), dim=1)
+
+#             lstm_out, hidden = self.lstm(seq)           
+#             pred = self.linear(lstm_out)
+#             out = torch.cat((out, out[:, -1:, :] + pred[:, -1: , :]), dim=1)
+            
+#         for t in range(self.ws, one_full_traj.size(dim=1) - self.ws):
+
+#             seq = torch.cat((one_full_traj[:, t : t + self.ws, 0:1], out[:, t - self.ws : t , :]), dim=2)
+            
+#             lstm_out, hidden = self.lstm(seq)           
+#             pred = self.linear(lstm_out)
+
+#             out = torch.cat((out, out[:, t-1:t, :] + pred[:, -1: , :]), dim=1)
+
+#         return out, hidden 
+
+#     def simple_forward(self, seq):
+
+#         lstm_out, hidden = self.lstm(seq)           
+#         pred = self.linear(lstm_out)
+
+#         return pred, hidden         
+  
+# # OR MLP
+# class OR_MLP(nn.Module):
+    
+#     def __init__(self, input_size=3, hidden_size = 6, l_num=1, output_size=2, act_fn="tanh", act_at_end = None, timesteps=5):
+#         super(OR_MLP, self).__init__()
+        
+#         if act_fn == "tanh":
+#             fn = nn.Tanh()
+#         else:
+#             fn = nn.ReLU()
+
+#         hidden_sizes = [hidden_size for x in range(l_num)]
+#         # Create a list to hold the layers
+#         layers = []
+        
+#         # Input layer
+#         layers.append(nn.Linear(input_size, hidden_sizes[0]))
+#         layers.append(fn)
+        
+#         # Hidden layers
+#         for i in range(1, len(hidden_sizes)):
+#             layers.append(nn.Linear(hidden_sizes[i-1], hidden_sizes[i]))
+#             layers.append(fn)
+        
+#         # Output layer
+#         layers.append(nn.Linear(hidden_sizes[-1], output_size))
+        
+#         #Try final non linearity:
+#         if act_at_end != None:
+#             if act_at_end == "tanh":
+#                 layers.append(nn.Tanh())
+#             if act_at_end == "relu":
+#                 layers.append(nn.ReLU())
+#             if act_at_end == "sigmoid":
+#                 layers.append(nn.Sigmoid())
+        
+#         # Use nn.Sequential to put together the layers
+#         self.network = nn.Sequential(*layers)
+#         self.ws = timesteps
+#         #self.timesteps = timesteps
+    
+#     def forward(self, one_full_traj):
+        
+#         seq = one_full_traj[:, 0:self.ws, :]
+
+#         #inp = torch.cat((seq[:, :self.ws,0], seq[:, :self.ws,1], seq[:, :self.ws,2]), dim=2)
+#         inp = torch.stack([torch.cat((a[:, 0], a[:, 1], a[:, 2])) for a in seq])
+#         pred = self.network(inp) 
+        
+#         out = one_full_traj[:, self.ws-1:self.ws, 1:] + pred.view(one_full_traj.size(dim=0),1,2)
+#         #out = one_full_traj[:, self.ws-1:self.ws, 1:]
+#         #print(out.size(),out)
+
+#         for t in range(1, self.ws): # für RK : range(1, self.ws + 2):
+
+
+#             tmp = torch.cat((one_full_traj[:,self.ws+(t-1):self.ws+(t-1)+(out.size(dim=1)), 0:1] , out[:, :, :]), dim=2)
+#             seq = torch.cat((one_full_traj[:, t:self.ws, :], tmp), dim=1)
+
+#             inp = torch.stack([torch.cat((a[:, 0], a[:, 1], a[:, 2])) for a in seq])
+
+#             pred = self.network(inp)
+
+#             out = torch.cat((out, out[:, -1:, 1:] + pred.view(one_full_traj.size(dim=0),1,2)), dim=1)
+
+#         for t in range(self.ws, one_full_traj.size(dim=1) - self.ws):
+
+#             seq = torch.cat((one_full_traj[:, t : t + self.ws, 0:1], out[:, t - self.ws : t , :]), dim=2)
+            
+#             inp = torch.stack([torch.cat((a[:, 0], a[:, 1], a[:, 2])) for a in seq])
+
+#             pred = self.network(inp)
+
+#             out = torch.cat((out, out[:, -1:, 1:] + pred.view(one_full_traj.size(dim=0),1,2)), dim=1)
+
+#         return out
+
+#     def simple_forward(self, seq):
+    
+#         #inp = torch.stack([torch.cat((a[:, 0], a[:, 1], a[:, 2])) for a in seq])
+#         # seq is already formatted for mlp input
+#         pred = self.network(seq) 
+
+#         return pred
+
+# OR TCN
+class OR_TCN(nn.Module):
+    def __init__(self, input_size, output_size, num_channels, kernel_size, dropout, windowsize=5):
+        super(OR_TCN, self).__init__()
+        self.tcn = TemporalConvNet(input_size, num_channels, kernel_size=kernel_size, dropout=dropout)
+        self.linear = nn.Linear(num_channels[-1], output_size)
+        self.init_weights()
+        self.ws = windowsize
+
+    def init_weights(self):
+        self.linear.weight.data.normal_(0, 0.01)
+
+    def forward(self, one_full_traj):
+
+        
+        # war falsch ! (hat trotzdem funktioniert???)
+        #seq = one_full_traj[:, 0:self.ws, :]
+        seq = one_full_traj[:, :, 0:self.ws]
+
+        y1 = self.tcn(seq)
+        pred = self.linear(y1[:, :, -1])
+        #only update next step
+        out = one_full_traj[:, 2:, self.ws-1] + pred
+        out = out.unsqueeze(-1)
+        #derivatie_sv = pred
+
+        for t in range(1, self.ws): # für RK : range(1, self.ws + 2):
+
+            tmp = torch.cat((one_full_traj[:, 0:2, self.ws+(t-1):self.ws+(t-1)+(out.size(dim=2))] , out[:, :, :]), dim=1)
+            seq = torch.cat((one_full_traj[:, :, t:self.ws], tmp), dim=2)
+
+            y1 = self.tcn(seq)
+            pred = self.linear(y1[:, :, -1])
+
+            next_step = out[:, :, t-1] + pred
+            next_step = next_step.unsqueeze(-1)
+
+            out = torch.cat((out, next_step), dim=2)
+
+            #derivatie_sv = torch.cat((derivatie_sv, pred), dim=2)
+
+        for t in range(self.ws, one_full_traj.size(dim=2) - self.ws):
+
+            seq = torch.cat((one_full_traj[:, 0:2, t : t + self.ws], out[:, :, t - self.ws : t]), dim=1)
+            
+            y1 = self.tcn(seq)
+            pred = self.linear(y1[:, :, -1])
+
+            next_step = out[:, :, t-1] + pred
+            next_step = next_step.unsqueeze(-1)
+
+            out = torch.cat((out, next_step), dim=2)
+
+            #derivatie_sv = torch.cat((derivatie_sv, pred[:, -1: , :]), dim=1)
+
+        return out
+    
+    def simple_forward(self, one_full_traj):
+
+        seq = one_full_traj[:, :, 0:self.ws]
+        y1 = self.tcn(seq)
+        pred = self.linear(y1[:, :, -1])
+
+        return pred
+
+
+### TCN helper classes ###
+class Chomp1d(nn.Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, :-self.chomp_size].contiguous()
+
+class TemporalBlock(nn.Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = torch.nn.utils.parametrizations.weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.conv2 = torch.nn.utils.parametrizations.weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
+        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.relu = nn.ReLU()
+        self.init_weights()
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x)
+        return self.relu(out + res)
+
+class TemporalConvNet(nn.Module):
+
+
+    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
+        super(TemporalConvNet, self).__init__()
+        layers = []
+        num_levels = len(num_channels)
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            in_channels = num_inputs if i == 0 else num_channels[i-1]
+            out_channels = num_channels[i]
+            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                     padding=(kernel_size-1) * dilation_size, dropout=dropout)]
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
+
---
+++ Full_System/get_data_fs.py
@@ -0,0 +1,128 @@
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import data for full system.
+
+# Structure : [Batchsize, Sequence Length, Values] (switches for TCN but not here!)
+# Values : [(time), u1, u2, p, s, v] (time is not returned!)
+# Normalized : "minmax" is default - data is always in [0, 1]
+ 
+#physical constants
+u1_max = 200  #Spannung in [V]              ... [0, 200]
+u1_min = 0
+u2_max = 200
+u2_min = 0
+p_max = 3.5*1e5 #Druck in [bar]             ... [1, 3.5]
+p_min = 1.0*1e5 #Umgebungsdruck in [bar]
+s_max = 0.605*1e-3     #Position [m]          ... [0, 0.0006]
+s_min = 0.0
+v_max = 0.6     #Geschwindigkeit in [m/s]   ... [-1.7, 1.7]
+v_min = -0.6    #Geschwindigkeit in [m/s]   ... [-1.7, 1.7]
+x_max = torch.tensor([u1_max, u2_max, p_max, s_max, v_max])
+x_min = torch.tensor([u1_min, u2_min, p_min, s_min, v_min])
+
+def normalize(data):
+
+    # This function normalizes any tensor of shape (batchsize, length, 5)
+    # maybe check if time is provided 
+
+    data = data.clone()
+    data = (data - x_min) / (x_max - x_min)
+    return data
+
+
+def normalize_invert(data):
+    
+    # This function inverts the minmax normalization back to the original scale.
+    data = data.clone()
+    data = data * (x_max - x_min) + x_min
+    return data
+
+
+def get_data(path, num_inits=0):
+
+    df = pd.read_csv(path)
+
+    #drop every second timestep
+    drop_half_timesteps = True
+    if drop_half_timesteps:
+     df = df.iloc[::2]
+
+    if num_inits>1:
+       df = df.iloc[:,0:6*num_inits]
+
+    #Reorder columns for familiar setup (t,u,x) here (t, p_b, s_b, w_b)
+    L = df.columns.to_list()
+    time_cols = L[0::6]
+    df = df.drop(time_cols, axis=1)
+
+    # columns are already in the right order.. just drop time cols
+    # u1_cols = L[1::6]
+    # u2_cols = L[2::6]
+    # pb_cols = L[3::6]
+    # sb_cols = L[4::6]
+    # wb_cols = L[5::6]
+    #new_col_order = [x for sub in list(zip(time_cols, u1_cols, u2_cols, pb_cols, sb_cols, wb_cols)) for x in sub]
+    #df= df[new_col_order]
+
+    tensor = torch.tensor(df.values)
+
+    a = num_inits if num_inits>0 else 500
+    a=int(len(df.columns.to_list())/5)
+
+    tensor = tensor.view(len(df),a,5).permute(1,0,2)
+    tensor = normalize(tensor)
+
+    return tensor
+
+
+def visualise(data, num_inits):
+
+    stepsize = 2e-5
+    time = np.linspace(0,data.size(dim=1)*stepsize, data.size(dim=1))
+    time = torch.tensor(time)
+
+    # if num_inits >= data.size(dim=0):
+        
+    #    print("index exceeds number of initial conditions -> random value chosen")
+    #    ids = np.random.randint(0,data.size(dim=0),1)
+    # else:
+    ids = [num_inits]    
+    
+    figure , axs = plt.subplots(5, 1, figsize=(16,9))
+    figure.tight_layout(pad=2.0) 
+    colors=["r","b","g","orange", "purple"]
+    for k, id in enumerate(ids):
+
+        if data[0,10,2] > 2:
+            axs[0].set_ylim(u1_min, u1_max)
+            axs[1].set_ylim(u2_min, u2_max)
+            axs[2].set_ylim(p_min, p_max)
+            axs[3].set_ylim(s_min, s_max)
+            axs[4].set_ylim(v_min, v_max)
+        else:
+           for i in range(5):
+              axs[i].set_ylim(0,1)
+        
+        axs[0].plot(time, data[id,:,0], label="voltage 1", color=colors[0], linewidth=2)
+        axs[1].plot(time, data[id,:,1], label="voltage 2", color=colors[1], linewidth=2)
+        axs[2].plot(time, data[id,:,2], label="pressure", color=colors[2], linewidth=2)
+        axs[3].plot(time, data[id,:,3], label="position", color=colors[3], linewidth=2)
+        axs[4].plot(time, data[id,:,4], label="speed", color=colors[4], linewidth=2)
+        axs[4].set_xlabel(f"time [s]    stepsize = 2e-5")
+
+        for i in range(5):
+            axs[i].grid(True)
+            axs[i].legend()
+        
+        #axs[2].set_title("pressure [Pa]")
+
+    plt.show()
+
+
+data = get_data(path=r"C:\Users\strasserp\Documents\ventil_lstm\data_fs\training_data_full_system.csv", num_inits=4)
+visualise(data, num_inits=3)
+visualise(normalize_invert(data), num_inits=3)
+
