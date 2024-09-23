abc13fa4602bd1fa5eeefc012297b4222e30e483
Gandalf3141 <paul.strasser98@gmail.com>
2024-09-23 13:57:43 +0200

    .

--- Experiment_Meassurements/meas_test_func_fs.py
+++ Experiment_Meassurements/meas_test_func_fs.py
@@ -1,3 +1,5 @@
+
+
 import torch
 from meas_get_data import *
 from meas_NN_classes import *
@@ -81,7 +83,73 @@ def plot_results(x, pred, rescale=False, window_size=1, sim_data_index=0):
     plt.legend()
     plt.show()
 
-def test(data, model, model_type="lstm", window_size=1 ,display_plots=False, numb_of_inits=1, fix_random=True, rescale=False, specific_index=-1):
+
+def plot_histogramm(error_position : list,
+                    error_pressure : list,
+                    error_position_simulink : list,
+                    error_pressure_simulink : list)->None:
+    
+    SMALL_SIZE = 15
+    MEDIUM_SIZE = 25
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=15)    # legend fontsiz
+
+    params = {'axes.titlesize': 20}
+    plt.rcParams.update(params)
+
+    fig, (ax1, ax2) = plt.subplots(2, 1)#, figsize=(9,9))  # Create a figure with 2 subplots
+
+    bins = 5
+    ax1.hist(error_position, alpha=0.5, label='position neural net', color='red', edgecolor = "black", hatch = "//", linewidth=2)
+    ax1.hist(error_position_simulink, alpha=0.3, label='position simulink', color='green', edgecolor = "black", hatch = "||", linewidth=2)
+    
+    ax1.axvline(np.array(error_position).mean(), color='orange', linewidth=2, label=f"Mean {np.round(np.array(error_position).mean(),5)}: MSE position")
+    ax1.axvline(np.array(error_position_simulink).mean(), color='blue', linewidth=2, label=f"Mean {np.round(np.array(error_position_simulink).mean(),5)}: MSE position simulink")
+    
+    ax1.set_xlabel('MSE position')
+    ax1.legend()
+    ax1.grid(True)
+
+    # Plot C and D in the second subplot (ax2)
+    ax2.hist(error_pressure, bins=bins, alpha=0.5, label='pressure neural net', color='red', edgecolor = "black", hatch = "//", linewidth=2)
+    ax2.hist(error_pressure_simulink, bins=bins, alpha=0.3, label='pressure simulink', color='green', edgecolor = "black", hatch = "||", linewidth=2)
+
+    ax2.axvline(np.array(error_pressure).mean(), color='orange', linewidth=2, label=f"Mean {np.round(np.array(error_position).mean(),5)}: MSE pressure")
+    ax2.axvline(np.array(error_pressure_simulink).mean(), color='blue', linewidth=2, label=f"Mean {np.round(np.array(error_position_simulink).mean(),5)}: MSE pressure simulink")
+    
+    ax2.set_xlabel('MSE pressure')
+    ax2.legend()
+    ax2.grid(True)
+
+    ax1.set_ylabel(f"frequency", fontsize=15)
+    ax2.set_ylabel(f"frequency", fontsize=15)
+
+    # Display the figure
+    plt.tight_layout()
+    plt.show()
+    
+    return
+
+# def test(data, model, model_type="lstm", window_size=1 ,display_plots=False,\
+#           numb_of_inits=1, fix_random=True, rescale=False, specific_index=-1, error_histogramm: bool = False):
+
+def test(
+    data, 
+    model, 
+    model_type: str = "lstm", 
+    window_size: int = 1, 
+    display_plots: bool = False, 
+    numb_of_inits: int = 1, 
+    fix_random: bool = True, 
+    rescale: bool = False, 
+    specific_index: int = -1, 
+    error_histogramm: bool = False
+) -> float:
+
 
     if fix_random:
      np.random.seed(1234)
@@ -97,11 +165,22 @@ def test(data, model, model_type="lstm", window_size=1 ,display_plots=False, num
     if specific_index >= 0:
         data = data[specific_index:specific_index+1,:, :]
     else :
-        data = data[ids,:, :]   
+        if not plot_histogramm: #use all data for histogramm
+         data = data[ids,:, :]   
+
     loss_fn = nn.MSELoss()
     timesteps = data.size(dim=1)
 
     total_loss = 0
+
+    error_position = []
+    error_pressure = []
+
+    path_test_data_simulink = r"C:\Users\strasserp\Documents\ventil_lstm\Experiment_Meassurements\Messungen\Messdaten_Simulink_Vergleich.csv"
+    test_data_simulink = get_data(path_test_data_simulink, num_inits=0)
+    error_position_simulink = []
+    error_pressure_simulink = []
+
     for i, x in enumerate(data):
 
         with torch.inference_mode():
@@ -122,10 +201,21 @@ def test(data, model, model_type="lstm", window_size=1 ,display_plots=False, num
 
                 total_loss += loss_fn(pred[window_size:, 2:], x[0, window_size:, 2:]).detach().cpu().numpy()
 
+                error_position.append(loss_fn(pred[window_size:, 2:3], x[0, window_size:, 2:3]).detach().cpu().numpy())
+                error_pressure.append(loss_fn(pred[window_size:, 3:4], x[0, window_size:, 3:4]).detach().cpu().numpy())
+
+            
+                error_position_simulink.append(loss_fn(test_data_simulink[i, window_size:data.size(dim=1), 2:3], x[0, window_size:, 2:3]).detach().cpu().numpy())
+                error_pressure_simulink.append(loss_fn(test_data_simulink[i, window_size:data.size(dim=1), 3:4], x[0, window_size:, 3:4]).detach().cpu().numpy())
+
                 if display_plots:
                     if specific_index>=0:
                         plot_results(x, pred, rescale=rescale, window_size=window_size, sim_data_index=specific_index)
                     else:
                         plot_results(x, pred, rescale=rescale, window_size=window_size, sim_data_index=ids[i])
+    
+    if error_histogramm:
+        
+        plot_histogramm(error_position, error_pressure, error_position_simulink, error_pressure_simulink)
 
     return total_loss/data.size(0)
